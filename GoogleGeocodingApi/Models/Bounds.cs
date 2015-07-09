00ddbe4099f5adaa71905b2e05e0b94bf46db0a8
Seifer <seed-seifer@ya.ru>
2015-07-09 20:05:29 +0300

    Added GetApproximateCenter method to Bounds

--- GoogleGeocodingApi/Models/Bounds.cs
+++ GoogleGeocodingApi/Models/Bounds.cs
@@ -6,6 +6,25 @@ namespace GoogleGeocodingApi
 	{
 		public Location Northeast { get; set; }
 		public Location Southwest { get; set; }
+
+		public Location GetApproximateCenter ()
+		{
+			if (Northeast == null || Southwest == null) {
+				return null;
+			}
+
+			double latitudeCenter = (Southwest.Latitude + Northeast.Latitude) / 2;
+			double longitudeCenter;
+			if (Southwest.Longitude <= Northeast.Longitude) 
+			{
+				longitudeCenter = (Northeast.Longitude + Southwest.Longitude) / 2;
+			}
+			else 
+			{
+				longitudeCenter = (Northeast.Longitude + 360 + Southwest.Longitude) / 2;
+			}
+			return new Location (latitudeCenter, longitudeCenter);
+		}
 	}
 }
 
